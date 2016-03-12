d569eaf176960a62d9adc3dc522e1e066502cd5d
Kudin Sasha <kudinsasha@gmail.com>
2016-03-12 17:42:01 +0300

    интерфейс отчётов - заменил сторонние библиотеки

--- local/components/kudin/report/templates/.default/script.js
+++ local/components/kudin/report/templates/.default/script.js
@@ -1,16 +1,55 @@
 $(function () {
-
-    $('#reservation').daterangepicker(null, function (start, end, label) {
-        console.log(start.toISOString(), end.toISOString(), label);
+    $('#reservation').daterangepicker({
+        ranges: {
+            'Сегодня': [moment(), moment()],
+            'Вчера': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Последние 7 дней': [moment().subtract(6, 'days'), moment()],
+            'Последние 30 дней': [moment().subtract(29, 'days'), moment()],
+            'Этот месяц': [moment().startOf('month'), moment().endOf('month')],
+            'Прошлый месяц': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        locale: { 
+            format: 'DD.MM.YYYY',
+            separator: " - ",
+            applyLabel: "Применить",
+            cancelLabel: "Отмена",
+            fromLabel: "От",
+            toLabel: "Дo",
+            customRangeLabel: "Выбрать",
+            daysOfWeek: [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            monthNames: [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            firstDay: 1
+        }
     });
-            
+
     $('#userid').change(function () {
         val = $(this).val();
         $('#userprojects').html('');
-        for(id in usersToProjects[val]) {
-            $('#userprojects').append('<option value="' + id + '">' + projects[usersToProjects[val][id]].name + '</option>'); 
+        for (id in usersToProjects[val]) {
+            $('#userprojects').append('<option selected value="' + id + '">' + projects[usersToProjects[val][id]].name + '</option>');
         }
-    }); 
+    });
     $('#userid').change();
-}); 
+});
  
--- local/components/kudin/report/templates/.default/template.php
+++ local/components/kudin/report/templates/.default/template.php
@@ -4,18 +4,49 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 ?>
 <script>
     projects = {
-        <? foreach($arResult['PROJECTS'] as $project) { ?>
-           <?=$project['ID']?>: {name: '<?=$project['NAME'];?>'} ,
-        <? } ?>
+<? foreach ($arResult['PROJECTS'] as $project) { ?>
+    <?= $project['ID'] ?>: {name: '<?= $project['NAME']; ?>'},
+<? } ?>
     };
     usersToProjects = {
-        <? foreach($arResult['USER_TO_PROJECT'] as $user => $projects) { ?>
-            <?=$user;?>: [<?=implode(',', $projects)?>] , 
-        <? } ?>
+<? foreach ($arResult['USER_TO_PROJECT'] as $user => $projects) { ?>
+    <?= $user; ?>: [<?= implode(',', $projects) ?>],
+<? } ?>
     };
 </script>
 <div class="row report">
-    <div class="col-md-12">
+    <div class="col-md-4">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2>Данные для отчёта</h2>  
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content"> 
+                <div class="row">
+                    <div class="col-md-12 col-sm-12">
+                        <form method="POST">
+                            <select class="form-control" id="userid">
+                                <?
+                                foreach ($arResult['USERS'] as $user) {
+                                    ?>
+                                    <option value="<?= $user['ID']; ?>"><?= $user['FULL_NAME']; ?></option>
+                                    <?
+                                }
+                                ?> 
+                            </select>    
+                            <div class="input-prepend input-group">
+                                <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>
+                                <input type="text" name="reservation" id="reservation" class="form-control" value="<?= date('d.m.Y'); ?> - <?= date('d.m.Y'); ?>" />
+                            </div> 
+                            <select class="form-control" id="userprojects" multiple="multiple"></select>  
+                            <button type="submit" class="btn btn-success"><i class="fa fa-table"></i> Создать отчёт</button>                        
+                        </form>
+                    </div>    
+                </div>
+            </div> 
+        </div>
+    </div> 
+    <div class="col-md-8">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Отчёт</h2>  
@@ -23,37 +54,11 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
             </div>
             <div class="x_content"> 
                 <div class="row">
-                    <div class="col-md-4 col-sm-4">
-                        <select class="form-control" id="userid">
-                            <?
-                            foreach($arResult['USERS'] as $user) {
-                                ?>
-                                <option value="<?=$user['ID'];?>"><?=$user['FULL_NAME'];?></option>
-                                <?
-                                }
-                            ?> 
-                        </select>   
-
-                        <div class="input-prepend input-group">
-                            <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>
-                            <input type="text" style="width: 200px" name="reservation" id="reservation" class="form-control" value="03/18/<?=date('Y');?> - 03/23/<?=date('Y');?>" />
-                        </div>
-
-                    </div> 
-                    <div class="col-md-4 col-sm-4">
-                        <select class="form-control" id="userprojects" multiple="multiple"></select> 
-                    </div> 
-                    <div class="col-md-4 col-sm-4">
-          
-                    </div> 
-                </div>    
-                
-                <div class="row">
                     <div class="col-md-12 col-sm-12">
- 
-                    </div> 
+
+                    </div>    
                 </div>
             </div> 
         </div>
-    </div>
+    </div> 
 </div>
